3e7a98115a4a291904b00ecbc412655f8e86db90
Vishwas <VishwasDubey150@gmail.com>
2023-01-05 04:32:33 +0530

    starting again

--- app/src/main/java/com/example/drawit/DrawingView.kt
+++ app/src/main/java/com/example/drawit/DrawingView.kt
@@ -1,105 +1,116 @@
 package com.example.drawit
-
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
-import java.util.jar.Attributes
 
-class DrawingView(context:Context,attrs:AttributeSet):View(context,attrs) {
+class DrawingView(context: Context,attrs:AttributeSet): View(context,attrs) {
+    private var mDrawPath: CustomPath? =null // An variable of CustomPath inner class to use it further.
+    private var mCanvasBitmap: Bitmap? = null // An instance of the Bitmap.
+    private var mDrawPaint: Paint? =null // The Paint class holds the style and color information about how to draw geometries, text and bitmaps.
+    private var mCanvasPaint: Paint? = null // Instance of canvas paint view.
+    private var mBrushSize: Float = 0.toFloat() // A variable for stroke/brush size to draw on the canvas.
+    private var color = Color.BLACK // A variable to hold a color of the stroke.
+    private var canvas: Canvas? = null
 
-    private var mDrawPath:CustomPath?=null
-    private var mCanvasBitmap:Bitmap?=null
-    private var mDrawPaint:Paint?=null
-    private var mCanvasPaint:Paint?=null
-    private var mBrushSize:Float=0.toFloat()
-    private var color=Color.BLACK
-    private var canvas: Canvas?=null
-    private val mPaths=ArrayList<CustomPath>()
+
+    private val mPaths = ArrayList<CustomPath>() // ArrayList for Paths
+    private val mUndoPaths = ArrayList<CustomPath>() // ArrayList for Paths
 
     init {
         setUpDrawing()
     }
 
+    fun onclickUndo()
+    {
+        if(mPaths.size>0)
+        {
+            mUndoPaths.add(mPaths.removeAt(mPaths.size-1))
+            invalidate()
+        }
+    }
     private fun setUpDrawing() {
-        mDrawPaint= Paint()
-        mDrawPath=CustomPath(color,mBrushSize)
-        mDrawPaint!!.color=color
-        mDrawPaint!!.style=Paint.Style.STROKE
-        mDrawPaint!!.strokeJoin=Paint.Join.ROUND
-        mDrawPaint!!.strokeCap=Paint.Cap.ROUND
-        mCanvasPaint= Paint(Paint.DITHER_FLAG)
-        mBrushSize=20.toFloat()
+        mDrawPaint = Paint()
+        mDrawPath = CustomPath(color, mBrushSize)
+        mDrawPaint?.color = color
+        mDrawPaint?.style = Paint.Style.STROKE // This is to draw a STROKE style
+        mDrawPaint?.strokeJoin = Paint.Join.ROUND // This is for store join
+        mDrawPaint?.strokeCap = Paint.Cap.ROUND // This is for stroke Cap
+        mCanvasPaint = Paint(Paint.DITHER_FLAG) // Paint flag that enables dithering when blitting.
     }
 
-    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
-        super.onSizeChanged(w, h, oldw, oldh)
-        mCanvasBitmap= Bitmap.createBitmap(w,h,Bitmap.Config.ARGB_8888)
-        canvas= Canvas(mCanvasBitmap!!)
+    override fun onSizeChanged(w: Int, h: Int, wprev: Int, hprev: Int) {
+        super.onSizeChanged(w, h, wprev, hprev)
+        mCanvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+        canvas = Canvas(mCanvasBitmap!!)
     }
 
-    //change canvas to canvas? if fails
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        canvas.drawBitmap(mCanvasBitmap!!,0f,0f,mCanvasPaint)
-        for (path in mPaths)
-        {
-            mDrawPaint!!.strokeWidth=mDrawPath!!.brushThickness
-            mDrawPaint!!.color=mDrawPath!!.color
-            canvas.drawPath(path,mDrawPaint!!)
+    mCanvasBitmap?.let {
+            canvas.drawBitmap(it, 0f,   0f, mCanvasPaint)
+        }
+        for (p in mPaths) {
+            mDrawPaint?.strokeWidth = p.brushThickness
+            mDrawPaint?.color = p.color
+            canvas.drawPath(p, mDrawPaint!!)
         }
 
-
-        if(!mDrawPath!!.isEmpty)
-        {
-            mDrawPaint!!.strokeWidth=mDrawPath!!.brushThickness
-            mDrawPaint!!.color=mDrawPath!!.color
-            canvas.drawPath(mDrawPath!!,mDrawPaint!!)
+        if (!mDrawPath!!.isEmpty) {
+            mDrawPaint?.strokeWidth = mDrawPath!!.brushThickness
+            mDrawPaint?.color = mDrawPath!!.color
+            canvas.drawPath(mDrawPath!!, mDrawPaint!!)
         }
     }
 
-    override fun onTouchEvent(event: MotionEvent?): Boolean {
-        val touchX=event?.x
-        val touchY=event?.y
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        val touchX = event.x // Touch event of X coordinate
+        val touchY = event.y // touch event of Y coordinate
 
-        when(event?.action)
-        {
-            MotionEvent.ACTION_DOWN ->{
-                mDrawPath!!.color=color
-                mDrawPath!!.brushThickness=mBrushSize
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                mDrawPath?.color = color
+                mDrawPath?.brushThickness = mBrushSize
+                mDrawPath?.reset() // Clear any lines and curves from the path, making it empty.
+                mDrawPath?.moveTo(
+                    touchX,
+                    touchY
+                ) // Set the beginning of the next contour to the point (x,y).
+            }
 
-                mDrawPath!!.reset()
-                if(touchX!=null)
-                {
-                    if(touchY!=null)
-                    {
-                        mDrawPath!!.moveTo(touchX,touchY)
-                    }
-                }
+            MotionEvent.ACTION_MOVE -> {
+                mDrawPath?.lineTo(
+                    touchX,
+                    touchY
+                ) // Add a line from the last point to the specified point (x,y).
             }
-            MotionEvent.ACTION_MOVE->{
-                if(touchX!=null)
-                {
-                    if(touchY!=null)
-                    {
-                        mDrawPath!!.lineTo(touchX,touchY)
-                    }
-                }
+
+            MotionEvent.ACTION_UP -> {
+
+                mPaths.add(mDrawPath!!) //Add when to stroke is drawn to canvas and added in the path arraylist
+
+                mDrawPath = CustomPath(color, mBrushSize)
             }
-            MotionEvent.ACTION_UP ->{
-                mPaths.add(mDrawPath!!)
-                mDrawPath=CustomPath(color,mBrushSize)
-            }
-            else-> return false
+            else -> return false
         }
-        invalidate()
 
+        invalidate()
         return true
     }
 
-    internal inner class CustomPath(var  color: Int,var brushThickness:Float): Path()
-    {
-
+    fun setSizeForBrush(newSize: Float) {
+        mBrushSize = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP, newSize,
+            resources.displayMetrics
+        )
+        mDrawPaint?.strokeWidth = mBrushSize
     }
+
+    fun setColor(newColor: Int) {
+        color = Color.parseColor(newColor.toString())
+        mDrawPaint?.color = color
+    }
+    internal inner class CustomPath(var color:Int,var brushThickness:Float):Path()
 }
--- app/src/main/java/com/example/drawit/MainActivity.kt
+++ app/src/main/java/com/example/drawit/MainActivity.kt
@@ -1,14 +1,35 @@
 package com.example.drawit
 
-import android.content.Intent
+import android.app.Dialog
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private var drawingView: DrawingView? = null
+
+    private var mImageButtonCurrentPaint: ImageButton? =
+        null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
+
+        drawingView = findViewById(R.id.drawingview)
+        drawingView?.setSizeForBrush(7.toFloat())
+
+        val undo:Button=findViewById(R.id.undo)
+        undo.setOnClickListener{
+            drawingView?.onclickUndo()
+        }
+
+
+
     }
 }
